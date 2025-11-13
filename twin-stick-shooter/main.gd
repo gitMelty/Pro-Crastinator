fd85e66f1af026c063f1d201c5e1eb2729706839
gitMelty <niko.va.aro@gmail.com>
2025-11-13 11:46:21 +0200

    Added mobs
    Still need to add Shooting

---
+++ twin-stick-shooter/main.gd
@@ -0,0 +1,34 @@
+extends Node
+
+@export var mob_scene: PackedScene
+
+func _ready():
+	new_game()
+
+
+
+func new_game():
+	#$Node2D.start($StartPosition.position)
+	$StartTimer.start()
+	get_tree().call_group("mobs", "queue_free")
+
+func _on_start_timer_timeout():
+	$MobTimer.start()
+
+func _on_mob_timer_timeout() -> void:
+	var mob = mob_scene.instantiate()
+	
+	var mob_spawn_location = $MobPath/MobSpawnLocation
+	mob_spawn_location.progress_ratio = randf()
+	
+	mob.position = mob_spawn_location.position
+	
+	var direction = mob_spawn_location.rotation + PI / 2
+	
+	direction += randf_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+	
+	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+	mob.linear_velocity = velocity.rotated(direction)
+	
+	add_child(mob)
--- twin-stick-shooter/player.gd
+++ twin-stick-shooter/player.gd
@@ -1,18 +1,33 @@
 extends CharacterBody2D
+var screen_size
 
 @export var speed = 400
 var pos = get_local_mouse_position() - position
 
+func start():
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
 
-func get_input():
+
+func get_input(): #8-way movement with WASD
 	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = input_direction * speed
 	
-func _physics_process(_delta):
+func _physics_process(_delta): 
 	get_input()
 	move_and_slide()
 	get_rotate()
 
-func get_rotate():
+func get_rotate(): #rotation looking at mouse, can be changed to a more mobile friendly version if anyone knows how
 	rotation = get_global_mouse_position().angle_to_point(position)
 	velocity = transform.x * Input.get_axis("rotate_down", "rotate_up") * speed
+
+signal hit
+
+
+func  _on_area_2d_body_entered(_body: RigidBody2D) -> void:
+	#hiding the player and emitting hit signal when colliding with mobs
+	hide()
+	hit.emit()
+	$CollisionShape2D.set_deferred("disabled", true)
